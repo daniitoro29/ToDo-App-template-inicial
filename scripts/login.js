a06f0ed8510200ed21e3149dbce8afd1f345f22f
daniitoro29 <daniitoro29@gmail.com>
2024-06-13 22:43:27 -0500

    signup and login added

--- scripts/login.js
+++ scripts/login.js
@@ -1,33 +1,43 @@
-window.addEventListener('load', function () {
-    /* ---------------------- obtenemos variables globales ---------------------- */
-   
-    
+window.addEventListener("load", function () {
+  /* ---------------------- obtenemos variables globales ---------------------- */
+  const urlLogin = "https://todo-api.digitalhouse.com/v1/users/login";
+  const form = document.forms[0];
+  const email = document.querySelector("#inputEmail");
+  const password = document.querySelector("#inputPassword");
 
-
-
-    /* -------------------------------------------------------------------------- */
-    /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
-    /* -------------------------------------------------------------------------- */
-    form.addEventListener('submit', function (event) {
-       
-        
-
-
-
-    });
-
-
-    /* -------------------------------------------------------------------------- */
-    /*                     FUNCIÓN 2: Realizar el login [POST]                    */
-    /* -------------------------------------------------------------------------- */
-    function realizarLogin(settings) {
-       
-
-
-
-
-        
+  /* -------------------------------------------------------------------------- */
+  /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+  /* -------------------------------------------------------------------------- */
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const data = {
+      email: email.value,
+      password: password.value,
     };
 
+    const settings = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    realizarLogin(settings);
+  });
 
+  /* -------------------------------------------------------------------------- */
+  /*                     FUNCIÓN 2: Realizar el login [POST]                    */
+  /* -------------------------------------------------------------------------- */
+  function realizarLogin(settings) {
+    fetch(urlLogin, settings)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data ****", data);
+        if (data.jwt) {
+          localStorage.setItem("jwt", data.jwt);
+          location.replace("./mis-tareas.html");
+        }
+      })
+      .catch((error) => console.error(error));
+  }
 });
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,31 +1,48 @@
-window.addEventListener('load', function () {
-    /* ---------------------- obtenemos variables globales ---------------------- */
-   
+window.addEventListener("load", function () {
+  /* ---------------------- obtenemos variables globales ---------------------- */
+  const urlSignUp = "https://todo-api.digitalhouse.com/v1/users";
+  const form = document.forms[0];
+  const firstName = document.querySelector("#inputNombre");
+  const lastName = document.querySelector("#inputApellido");
+  const email = document.querySelector("#inputEmail");
+  const password = document.querySelector("#inputPassword");
 
-
-    
-
-    /* -------------------------------------------------------------------------- */
-    /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
-    /* -------------------------------------------------------------------------- */
-    form.addEventListener('submit', function (event) {
-      
-
-
-
-
-    });
-
-    /* -------------------------------------------------------------------------- */
-    /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
-    /* -------------------------------------------------------------------------- */
-    function realizarRegister(settings) {
-        
-
-
-
-
+  /* -------------------------------------------------------------------------- */
+  /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+  /* -------------------------------------------------------------------------- */
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const data = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      password: password.value,
     };
+    const settings = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    realizarRegister(settings);
+  });
 
+  /* -------------------------------------------------------------------------- */
+  /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
+  /* -------------------------------------------------------------------------- */
+  function realizarRegister(settings) {
+    console.log("settings", settings);
+    fetch(urlSignUp, settings)
+      .then((response) => {
+        
+       return response.json()
+      })
+      .then((data) => {
 
+        if (data.jwt) {
+          localStorage.setItem("jwt", data.jwt);
+        }
+      });
+  }
 });
